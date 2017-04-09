81e41aa8b5bd22253b675f48e5f055a9c81d4f64
Karl Andersson <Karl Andersson>
2017-04-09 23:04:51 +0200

    Created Guest model and index view

--- www/models.py
+++ www/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Guest(models.Model):
+    name = models.CharField(max_length=40)
+    rsvp_answer = models.CharField(max_length=40)
+    rsvp_datetime = models.TimeField(auto_created=True)
+    email = models.EmailField(max_length=100)
+    dietary_restrictions = models.TextField()
+    shuttle = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
