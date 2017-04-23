f1b2cbfcd2afe15156e5300a405e6b5f839e3562
Karl Andersson <Karl Andersson>
2017-04-23 22:38:59 +0200

    HTML and forms

--- www/models.py
+++ www/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Guest(models.Model):
-    name = models.CharField(max_length=40)
-    rsvp_answer = models.CharField(max_length=40)
-    rsvp_datetime = models.TimeField(auto_created=True)
+    RSVP_ANSWER_CHOICES = (('Kommer', 'Kommer'), ('Kommer Inte', 'Kommer Inte'))
+    SHUTTLE_CHOICES = (
+    ('NEJ', 'Nej'), ('DIT', 'Bara dit'), ('DIT2300', 'Dit och hem 23:00'), ('DIT0030', 'Dit och hem 00:30'),
+    ('DIT0200', 'Dit och hem 02:00'))
+
+    name = models.CharField(max_length=40, verbose_name='Namn (För- och Efternamn)')
+    rsvp_answer = models.CharField(max_length=40, verbose_name='OSA', choices=RSVP_ANSWER_CHOICES)
+    rsvp_datetime = models.TimeField(auto_now_add=True, blank=True)
     email = models.EmailField(max_length=100)
-    dietary_restrictions = models.TextField()
-    shuttle = models.CharField(max_length=40)
+    dietary_restrictions = models.TextField(verbose_name='Specialkost')
+    shuttle = models.CharField(max_length=40, choices=SHUTTLE_CHOICES, verbose_name='Vill du åka buss? (om du vill åka buss på hemvägen ge gärna en gissning på vilken buss du vill ta)')
 
     def __str__(self):
         return self.name
--- www/views.py
+++ www/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .forms import GuestForm
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    if (request.method == 'POST'):
+        form = GuestForm(data=request.POST)
+        if form.is_valid:
+            form.save()
+    else:
+        form = GuestForm()
+    return render(request, 'index.html', {'form':form})
